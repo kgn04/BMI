4eb863fd3d78e699fde36bd146255b46ae27f7a6
Kacper Gaudyn <kacper.gaudyn@pwr.edu.pl>
2023-11-12 12:12:19 +0100

    menu works just fine.

--- app/src/main/java/com/example/bmi/MainActivity.kt
+++ app/src/main/java/com/example/bmi/MainActivity.kt
@@ -4,46 +4,36 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.example_menu, menu)
+        menuInflater.inflate(R.menu.top_menu, menu)
         return true
     }
-
-    fun onGroupItemClick(item: MenuItem) {
-        // One of the group items (using the onClick attribute) was clicked.
-        // The item parameter passed here indicates which item it is.
-        // All other menu item clicks are handled by Activity.onOptionsItemSelected.
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection.
+        return when (item.itemId) {
+            R.id.history -> {
+                //newGame()
+                true
+            }
+            R.id.about_author -> {
+                //showHelp()
+                true
+            }
+            R.id.units_switch -> {
+                //showHelp()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
-
-//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-//        val inflater: MenuInflater = menuInflater
-//        inflater.inflate(R.menu.top_menu, menu)
-//        return true
-//    }
-//
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        // Handle item selection.
-//        return when (item.itemId) {
-//            R.id.history -> {
-//                //newGame()
-//                true
-//            }
-//            R.id.about_author -> {
-//                //showHelp()
-//                true
-//            }
-//            R.id.units_switch -> {
-//                //showHelp()
-//                true
-//            }
-//            else -> super.onOptionsItemSelected(item)
-//        }
-//    }
 }
